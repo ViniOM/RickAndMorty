aeb30795261ba9f5a8ba45a7df5c49c12401146a
Vinicios Gabriel <vini.webdeveloper@gmail.com>
2022-07-05 20:10:17 -0300

    feat: verificacao de ID e BodyObject

--- backend/middleware/personagem.middleware.js
+++ backend/middleware/personagem.middleware.js
@@ -3,18 +3,20 @@ const mongoose = require("mongoose");
 const validId = (req, res, next) => {
   const idParam = req.params.id;
   if (!mongoose.Types.ObjectId(idParam)) {
-    return res.status(400).send({ message: "ID Invalido!" });
+    return res
+      .status(400)
+      .send({ message: "ID Invalido!" });
   }
   next();
 };
 
 const validObjectBody = (req, res, next) => {
   const personagem = req.body;
-
   if (!personagem.name || !personagem.image) {
-    return res.status(400).send({ message: "Envie todos os campos do cadastro!"});
+    return res
+      .status(400)
+      .send({ message: "Envie todos os campos do cadastro!" });
   }
-
   next();
 };
 
